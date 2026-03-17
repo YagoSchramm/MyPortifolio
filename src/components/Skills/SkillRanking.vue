43e2f2f1a4163ac77d560e71e2db9a4f1c9ac61d
YagoSchramm <yrtoiu1515@gmail.com>
2026-03-17 20:17:22 -0300

    fix:Style of ranking of languages in the Skills tab fixed

--- src/components/Skills/SkillRanking.vue
+++ src/components/Skills/SkillRanking.vue
@@ -7,7 +7,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="backdrop-blur-md bg-white/40 border border-white/20 rounded-2xl p-6 shadow-lg space-y-6">
+  <div class="backdrop-blur-md  mb-8 bg-white/40 border border-white/20 rounded-2xl p-6 shadow-lg space-y-6">
     <SkillBar
       v-for="skill in skills"
       :key="skill.name"
